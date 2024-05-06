dcfaaba0fed66aec80f96164cbc2a500e03f93ab
Aeron Red Celajes <142370807+AeronRedCelajes@users.noreply.github.com>
2024-05-07 00:25:15 +0800

    Add files via upload

---
+++ Study PHP Basic (Function and Array)/functions.php
@@ -0,0 +1,32 @@
+<?php 
+#php functions
+/*
+	function message(){
+		echo "My First user define function";
+	}
+
+	message();
+*/
+
+##PHP function arguments
+/*
+function studname($name){
+    echo "$name <br/>";
+}
+
+studname("Juan Dela Cruz");
+studname("Peter Simion");
+studname("Jack Cruz");
+studname("Jane Doe");
+*/
+
+##PHP Functions with Returning values
+
+function addthis($num1, $num2, $num3){
+    $sumis = $num1 + $num2 + $num3;
+    return $sumis;
+}
+
+echo addthis(5,2,5);
+
+?>
